f16306b221d98d81841ebcf29406c65a5a3c1435
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-22 00:44:27 +0500

    responsiveness in allgoals component added

--- goalapp-frontend/src/components/Goal.jsx
+++ goalapp-frontend/src/components/Goal.jsx
@@ -31,20 +31,20 @@ const Goal = (props) => {
       {/* display goals in one card */}
       <div className="flex justify-center items-center  p-6 mt-[-45px] w-4/5">
         <div className="my-[20px] w-full">
-          <h2 className="text-xl font-bold mb-2 text-gray-800">
+          <h2 className="text-300 font-bold mb-2 text-gray-800 md:text-xl">
             {props.goal.text}
           </h2>
           <p className="text-600 text-gray-800">{props.goal.description}</p>
           {/* remove button */}
         </div>
         <button 
-        className="w-1/2 py-3 bg-red-600 text-white rounded hover:bg-red-500 m-2"
+        className="w-full py-3 bg-red-600 text-white rounded hover:bg-red-500 m-2 md:w-1/2"
         onClick={() => removeGoal()}
         >
           Remove
         </button>
         {/* Update Goal */}
-        <Link to='/updategoal' className="w-1/2">
+        <Link to='/updategoal' className="w-full md:w-1/2">
         <button 
         className="w-full py-3 bg-blue-600 text-white rounded hover:bg-blue-500 m-2"
         // on click update goal, pass the goal id, 
